297258f2f610da4358e81b7e4f762221d47c0367
john <john@IGZ20004JOHNS>
2019-04-02 18:53:35 +0530

    controller methods

--- clean-city-mission/src/main/java/com/cleancity/controllers/UserController.java
+++ clean-city-mission/src/main/java/com/cleancity/controllers/UserController.java
@@ -3,7 +3,12 @@ package com.cleancity.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +25,25 @@ public class UserController {
 	@GetMapping
 	public List<UserDetails> findAllUsers() {
 		return userService.findAll();
+	}
+
+	@GetMapping("/{userId}")
+	public UserDetails fetchUserDetailsByUserId(@PathVariable String userId) {
+		return null;
+	}
+
+	@PostMapping
+	public UserDetails saveUserDetails(@RequestBody UserDetails userData) {
+		return null;
+	}
+
+	@PutMapping
+	public UserDetails updateUserDetails(@RequestBody UserDetails userData) {
+		return null;
+	}
+
+	@DeleteMapping("/{userId}")
+	public boolean deleteUserDetails(@PathVariable String userId) {
+		return true;
 	}
 }
--- clean-city-mission/src/main/java/com/cleancity/user/service/UserServiceImpl.java
+++ clean-city-mission/src/main/java/com/cleancity/user/service/UserServiceImpl.java
@@ -23,9 +23,9 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public void addDummyData() {
 		
-		UserDetails u1 = new UserDetails("abcds", "john", (byte) 23, "473684gbdfg89");
-		UserDetails u2 = new UserDetails("abcdf", "sooraj", (byte) 25, "47368bdfbdf489");
-		UserDetails u3 = new UserDetails("abcdg", "xavier", (byte) 20, "47368bdxbf489");
+		UserDetails u1 = new UserDetails(null, "john", (byte) 23, "473684gbdfg89");
+		UserDetails u2 = new UserDetails(null, "sooraj", (byte) 25, "47368bdfbdf489");
+		UserDetails u3 = new UserDetails(null, "xavier", (byte) 20, "47368bdxbf489");
 		
 		userDetailRepo.save(u1);
 		userDetailRepo.save(u2);
